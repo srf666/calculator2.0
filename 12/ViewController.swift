88f89311103515624ff041a5f6339a4606dc649f
苏日甫 <1020343499@qq.com>
2016-06-27 11:33:04 +0800

    计算器2.0
    效仿老师上学期讲的计算器，用Swift完成了新的计算器。定义flag作为计数器，用来判断具体进行哪种运算。将第一次输入的值，也就是屏幕上显示的值
    （x.text）赋值给temp。现在temp的值就是第一次输入的值，然后按运算符，比如说加法，flag的值就为1，一点等号进入Swich判断，然
    后进行运算：temp=temp+(Double)(x.text!)，给temp赋值为第一次输入的值再加上第二次输入的值。这就完成了加法运算

---
+++ 12/ViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  12
+//
+//  Created by 20141105039 on 16/6/22.
+//  Copyright © 2016年 20141105039. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+          @IBOutlet weak var x: UITextField!
+          var flag=0
+          var temp=0.0
+          var pointval:Bool=false
+          @IBAction func add(sender: AnyObject) {
+                    if flag==0{
+                              temp=(Double)(x.text!)!
+                              x.text=""
+                              pointval = false
+                    }
+                    flag=1
+          }
+          @IBAction func sub(sender: AnyObject) {
+                    if flag==0{
+                              temp=(Double)(x.text!)!
+                              x.text=""
+                              pointval = false
+                    }
+                    flag=2
+          }
+          @IBAction func mul(sender: AnyObject) {
+                    if flag==0{
+                              temp=(Double)(x.text!)!
+                              x.text=""
+                              pointval = false
+                    }
+                    flag=3
+          }
+          @IBAction func div(sender: AnyObject) {
+                    if flag==0{
+                              temp=(Double)(x.text!)!
+                              x.text=""
+                              pointval = false
+                    }
+                    flag=4
+          }
+          @IBAction func calculator(sender: AnyObject) {
+                    switch flag {
+                    case 1:
+                              temp=temp+(Double)(x.text!)!
+                              x.text="\(temp)"
+                    case 2:
+                              temp=temp-(Double)(x.text!)!
+                              x.text="\(temp)"
+                    case 3:
+                              temp=temp*(Double)(x.text!)!
+                              x.text="\(temp)"
+                    case 4:
+                              if(Double)(x.text!)==0
+                              {
+                                 x.text="大兄弟，零不能这么玩啊"
+                              }
+                              else{
+                               temp=temp/(Double)(x.text!)!
+                               x.text="\(temp)"
+                              }
+                              
+                    default:
+                              break
+                    }
+          }
+          @IBAction func opposite(sender: AnyObject) {
+                    temp=(Double)(x.text!)!
+                    temp=(-temp)
+                    x.text="\(temp)"
+          }
+          @IBAction func Del(sender: AnyObject) {
+                    var str=x.text!
+                    if str != ""{
+                         str.removeAtIndex(str.endIndex.predecessor())
+                         x.text=str
+                    }
+                    else{
+                              x.text="0"
+                    }
+                    
+          }
+          @IBAction func ac(sender: AnyObject) {
+                    temp=0
+                    x.text=""
+                    flag=0
+          }
+          @IBAction func one(sender: AnyObject) {
+                    x.text=x.text!+"1"
+          }
+          @IBAction func two(sender: AnyObject) {
+                    x.text=x.text!+"2"
+          }
+          @IBAction func three(sender: AnyObject) {
+                    x.text=x.text!+"3"
+          }
+          @IBAction func four(sender: AnyObject) {
+                    x.text=x.text!+"4"
+          }
+          @IBAction func five(sender: AnyObject) {
+                    x.text=x.text!+"5"
+          }
+          @IBAction func six(sender: AnyObject) {
+                    x.text=x.text!+"6"
+          }
+          @IBAction func seven(sender: AnyObject) {
+                    x.text=x.text!+"7"
+          }
+          @IBAction func eight(sender: AnyObject) {
+                    x.text=x.text!+"8"
+          }
+          @IBAction func nine(sender: AnyObject) {
+                    x.text=x.text!+"9"
+          }
+          @IBAction func zero(sender: AnyObject) {
+                    x.text=x.text!+"0"
+          }
+          @IBAction func point(sender: AnyObject) {
+                    if(pointval == false){
+                      x.text=x.text!+"."
+                      pointval = true
+                    }
+                    else{
+                      x.text=x.text!+""
+                    }
+                    
+          }
+          override func viewDidLoad() {
+                    super.viewDidLoad()
+                    // Do any additional setup after loading the view, typically from a nib.
+          }
+
+          override func didReceiveMemoryWarning() {
+                    super.didReceiveMemoryWarning()
+                    // Dispose of any resources that can be recreated.
+          }
+
+
+}
+
